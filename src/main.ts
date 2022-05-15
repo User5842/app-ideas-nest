9950af33bf4826e44811f35602084e8e807bcbd1
Rafael Negron <r.negron1@umiami.edu>
2022-05-15 13:54:32 -0400

    add OpenAPI and compression;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import compression from "compression";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle("App Ideas Nest")
+    .setDescription("The app ideas server.")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.use(compression());
+
   await app.listen(process.env.PORT || 3000);
 }
 
